c8195b9e5f7a6f7c5f35a51746979d81d3da9e0e
lee <lsh3384@gmail.com>
2022-05-15 22:36:54 +0900

    grid edited

--- model/menu.js
+++ model/menu.js
@@ -3,11 +3,9 @@ const getConnection = require('../model/db')
 module.exports = exports = {};
 
 
-
-exports.get_left_menus = function(auth_cd) {
+exports.getAllMenus = function() {
   return new Promise((resolve, reject)=> {
-    var auth_cd_sql = '%' + auth_cd + '%';
-    getConnection().query('select * from menu_mng where menu_use_auth like ?',[auth_cd_sql], function(err, data) {
+    getConnection().query('select * from menu;',[], function(err, data) {
       console.log('get_left_menus');
       if(err) {
         reject(err);
--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 const { auth_check } = require("../model/auth_check");
-const { get_left_menus } = require("../model/menu");
+const { getAllMenus } = require("../model/menu");
 
 router.get("/main", async function (req, res) {
   let mnger_id = req.user;
@@ -27,24 +27,10 @@ router.get("/main", async function (req, res) {
 });
 
 router.get("/", async function (req, res) {
-  let mnger_id = req.user;
-  if (typeof mnger_id !== "undefined") {
-    var auth_cd = await auth_check(mnger_id);
-    console.log("main auth_cd : " + auth_cd);
-    var left_menus = await get_left_menus(auth_cd);
-    console.log(left_menus);
-  }
-
-  // nodejs에서 포함된 것을 걸러내는 경우.. 데이터베이스에서 포함된 것만 가져오는 것이 더 좋을 듯.
-  // let test_string = "AUTH0001, AUTH0002";
-  // console.log('include test:' + test_string.includes("AUTH0001"));
-
-  console.log("session_id in Main Page: " + req.sessionID);
-  console.log("user id in passport: " + req.user);
+  let menus = await getAllMenus();
 
   res.render("home", {
-    session_id: req.user,
-    left_menus: left_menus,
+    menus: menus,
   });
 });
 
